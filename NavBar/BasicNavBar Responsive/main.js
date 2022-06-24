e5ed9b40342489d18ed2a56c841c6387e372b1d2
lomps-droid <xand.douglass2@gmail.com>
2022-06-24 02:24:28 -0300

    Add new NavBar

---
+++ NavBar/BasicNavBar Responsive/main.js
@@ -0,0 +1,4 @@
+function tgActive(){
+    document.getElementsByClassName('nav__links')[0].classList.toggle('active')
+    document.getElementsByClassName('nav__icon')[0].classList.toggle('active')
+}
