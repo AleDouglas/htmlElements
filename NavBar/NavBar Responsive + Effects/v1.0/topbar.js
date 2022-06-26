bac32596313608bf2d882db589ff80cfdafa5baf
lomps-droid <xand.douglass2@gmail.com>
2022-06-26 19:45:36 -0300

    up

--- NavBar/NavBar Responsive + Effects/v1.0/topbar.js
+++
@@ -1,7 +0,0 @@
-function toggleButton() {
-    document.getElementsByClassName('navegar')[0].classList.toggle('active')
-    document.getElementsByClassName('conteudo')[0].classList.toggle('active')
-    document.getElementsByClassName('topbar')[0].classList.toggle('active')
-    document.getElementsByClassName('navBarHR')[0].classList.toggle('active')
-    document.getElementsByClassName('sociais')[0].classList.toggle('active')
-}
